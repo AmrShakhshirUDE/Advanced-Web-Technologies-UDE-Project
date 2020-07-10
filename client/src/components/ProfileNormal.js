d7fd1b6b927b4ea7371b92b86904f15870aa4232
AmrShakhshir <amrshakhshir@hotmail.com>
2020-07-10 15:56:42 +0200

    update user img

--- client/src/components/ProfileNormal.js
+++ client/src/components/ProfileNormal.js
@@ -15,50 +15,64 @@ export default class ProfileNormal extends Component{
             'username':decoded.identity.username,
             'email': decoded.identity.email,
             'image':decoded.identity.file,
-            'newImage':'',
             secondimage:'',
             Open:false,
             msg:'',
             'file':decoded.identity.file,
             uri:'',
-            'serverUrl': UrlContext._currentValue
+            'serverUrl': UrlContext._currentValue,
+            "result":"",
+            "resToken":"",
+            "mounted":false,
         }
     }
     
 
     componentDidMount () {
+        // this.logOut()
+        // const user = {
+        //     email: this.state.email,
+        //     password: this.state.password
+        // }
+        // this.login(user)
         const token = localStorage.usertoken
         const decoded = jwt_decode(token)
         this.setState({
             username: decoded.identity.username,
             email: decoded.identity.email,
-            image:this.state.newImage
-        })
-        this.getImage(this.state.file)
+            file:decoded.identity.file
+        },()=>this.getImage(this.state.file), console.log(this.state.file))
+        
     }
 
-    //     getFile(e) {
-    //     console.log('getfile')
-    //     const urlFile =this.state.serverUrl +'file/'+e
-    //     console.log(urlFile)
-    //     axios({
-    //       url: urlFile, //your url
-    //       method: 'Get',
-    //       responseType: 'blob',
-    //       data:{
-    //         "file":  e
-    //       } // important
-    //     }).then((response) => {
-    //        const url = window.URL.createObjectURL(new Blob([response.data]));
-    //        const link = document.createElement('a');
-    //        this.setState({
-    //            uri:urlFile
-    //        })
-    //        //link.href = url;
-    //        //link.setAttribute('download',fName); //or any other extension
-    //        //document.body.appendChild(link);
-    //       // link.click();
-    //     });
+    // logOut (e) {     
+    //     e.preventDefault()
+    //     localStorage.removeItem('usertoken')
+    //     this.props.history.push(`/login`)
+    // }
+    
+
+
+    // login = user => {
+    //     if (this.state.mounted){
+    //         this.logOut()
+    //     }
+    //     return axios
+    //         .post("users/login", {
+    //             email: user.email,
+    //             password: user.password
+    //         })
+    //         .then(response => {
+    //             this.setState({
+    //                 "result":response.data.msg
+    //             })
+    //             localStorage.setItem('usertoken', response.data.token)
+    //             return response.data.token
+                
+    //         })
+    //         .catch(err => {
+    //             console.log(err)
+    //         })
     // }
 
 
@@ -81,12 +95,14 @@ export default class ProfileNormal extends Component{
         const newFile =e.target.files[0]
         this.setState({
             file:newFile,
-        },(e)=>this.imageUpload(e));
+        // },(e)=>this.imageUpload(e));
+        },(e)=>this.getImage(this.state.file.name));
     }
 
-    imageUpload(e){
+    imageUpload(e,file){
         let email = this.state.email
-        let  file = this.state.file
+        // let  file = this.state.file
+        console.log(file)
         let formdata = new FormData()
         formdata.append('file',file)
         formdata.append('email',email)
@@ -99,7 +115,12 @@ export default class ProfileNormal extends Component{
             data:formdata
 
         }).then(response => {
-            this.getImage(file.name)                        
+            
+            this.setState({
+                // "result":response.data.msg,
+                // "mounted":true,
+            })
+            //  this.logOut(e)                       
         })
     }
    
@@ -124,28 +145,32 @@ export default class ProfileNormal extends Component{
                     accept="image/png, image/jpeg" onChange={(e)=>this.handleFile(e)}/>
                     {/* Change Image</button> */}
                 </div>
-                {/* <button onClick={this.imageUpload()}>upload</button> */}
+                <div  style={{"display":"flex", "justifyContent": "center", "alignItems": "center"}}>
+                    <button className="btn btn-primary mt-2" onClick={(e)=>this.imageUpload(e,this.state.file)}>update Image</button>                        
+                </div>
 
                 {/* <button onClick={()=>this.fileInput.click()}>Choose pictures</button><br/> */}
                <br></br>
-                <table className="table col-md-6 mx-auto">
-                    <tbody>
-                      <tr>
-                        <td>Username</td>
-                        <td onChange={this.change}>{this.state.username} </td>
-                      </tr>
-                      <tr>
-                        <td>Email</td>
-                        <td onChange={this.change}>{this.state.email} </td>
-                      </tr>
-                      <tr>
-                          <td></td>
-                          <td>
-                            <Button className="btn btn-primary mt-2" size="md" onClick={this.props.clickMe}>update</Button>
-                          </td>
+               <div className="">
+                    <table className="table col-md-6 mx-auto">
+                        <tbody>
+                        <tr>
+                            <td>Username</td>
+                            <td onChange={this.change}>{this.state.username} </td>
                         </tr>
-                        </tbody>
-                  </table>
+                        <tr>
+                            <td>Email</td>
+                            <td onChange={this.change}>{this.state.email} </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>
+                                <Button className="btn btn-primary mt-2" size="lg" onClick={this.props.clickMe}>update</Button>
+                            </td>
+                            </tr>
+                            </tbody>
+                    </table>
+               </div>
                 
 
                 
